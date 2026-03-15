b23e614c5974c983718f4aa78fbc27319757edfb
gusttavosants <gustavoluizsantosmelo@gmail.com>
2026-03-15 18:37:37 -0300

    Allow Netlify Origin to prevent CORS issues on Render backend

--- backend/main.py
+++ backend/main.py
@@ -47,6 +47,7 @@ ALLOWED_ORIGINS += [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://fin-control-peach.vercel.app",
+    "https://creative-cranachan-b54dcf.netlify.app",
 ]
 
 app.add_middleware(
